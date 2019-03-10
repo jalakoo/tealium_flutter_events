8945fcc8d818ec3de12e203096a38601d78987a3
Jason Koo <jalakoo@gmail.com>
2019-03-10 10:00:22 -0700

    Random events generator added to EventsManager

--- lib/managers/eventsManager.dart
+++ lib/managers/eventsManager.dart
@@ -1,23 +1,38 @@
 import '../models/event.dart';
 import 'managerMode.dart';
+import 'package:faker/faker.dart';
 
 // This class provides a list of events from a variety of different sources,
 // dependent on the ManagerMode setting
 class EventsManager {
   ManagerMode mode = ManagerMode.random;
-  List<Event> events;
+  List<Event> _events;
 
-  Event eventForIndex(int index) {
-    return events[index];
+  int count() {
+    return events().length;
   }
 
-  Event newEvents(ManagerMode currentMode) {
+  Event eventForIndex(int index) {
+    return events()[index];
+  }
+
+  List<Event> events() {
+    if (_events == null || _events.length == 0) {
+      print('EventsManager: Generating new events');
+      _events = _newEvents(mode);
+    }
+    return _events;
+  }
+
+  List<Event> _newEvents(ManagerMode currentMode) {
     switch (currentMode) {
       case ManagerMode.file:
 
         // Remove break to allow fallthrough
         break;
       case ManagerMode.random:
+        print('EventsManager: Getting new random events');
+        return _getEventsRandomly();
         break;
       case ManagerMode.prod:
         break;
@@ -31,6 +46,23 @@ class EventsManager {
 
   List<Event> _getEventsRandomly() {
     // Using faker library to populate content
+
+    int numberOfEvents = Faker().randomGenerator.integer(10, min: 3);
+    List<Event> result = List<Event>();
+    for (var i = 0; i < numberOfEvents; i++) {
+      result.add(_randomEvent());
+    }
+    print('EventsManager: Returning random events: ${result}');
+    return result;
+  }
+
+  Event _randomEvent() {
+    Event result = Event();
+    Faker faker = Faker();
+    result.name = faker.conference.name();
+
+    // TODO: Event details
+    return result;
   }
 
   List<Event> _getEventsFromProd() {
