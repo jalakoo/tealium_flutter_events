8c1280dd42b2dc0a5af97f2d3bf16c823315fdf0
Jason Koo <jalakoo@gmail.com>
2019-04-03 08:00:24 -0700

    User model add lastSessionStart property

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -4,4 +4,25 @@ import 'contact.dart';
 class User extends Contact {
   @required
   String authToken;
+  DateTime lastSessionStart;
+
+  User({this.authToken, this.lastSessionStart});
+
+  User.fromJson(Map<String, dynamic> json) {
+    Contact.fromJson(json);
+    authToken = json['auth_token'];
+    lastSessionStart = json['last_session_start'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = super.toJson();
+    data['auth_token'] = this.authToken;
+    data['last_session_start'] = this.lastSessionStart;
+    return data;
+  }
+
+  @override
+  String toString() {
+    return 'User {authToken: $authToken, lastSessionStart: $lastSessionStart}';
+  }
 }
