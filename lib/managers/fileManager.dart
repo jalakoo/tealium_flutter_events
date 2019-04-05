c021f1fe63509e8959e82449b15e1e8f39725539
christinatealium <christina.sund@tealium.com>
2019-04-05 15:01:45 -0700

    Add NewFileManager class at the end of fileManager.dart

--- lib/managers/fileManager.dart
+++ lib/managers/fileManager.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'dart:io';
 import 'dart:async' show Future;
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:path_provider/path_provider.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class FileManager {
   // Find the path to the documents directory for storage
@@ -54,4 +56,48 @@ class FileManager {
       throw e;
     }
   }
+}
+
+class NewFileManager {
+  final storage = new FlutterSecureStorage();
+
+  // Find the path to the documents directory for storage
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  // Create a reference to the file's locaion
+  Future<File> _localFile(String filename) async {
+    try {
+      final path = await _localPath;
+      final filepath = File('$path/$filename');
+      print('fileManager.dart: _localFile: $filepath');
+      return filepath;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  // load JSON from file
+  Future<String> loadJSON(String filename) async {
+    return await rootBundle.loadString('assets/$filename');
+  }
+
+  // serialize JSON, then write to storage
+  Future writeData(String filename) async {
+    final file = await _localFile(filename);
+    String contents = await file.readAsString();
+    final Map<String, dynamic> json = jsonDecode(contents);
+    json.forEach((key, value) async => await storage.write(key: key, value: value));
+  } 
+
+  Future<Map<String, String>> readAllValues() async {
+    return await storage.readAll();
+  }
+
+  Future<String> readValue(String key) async {
+    return await storage.read(key: key);
+  }
+
 }
